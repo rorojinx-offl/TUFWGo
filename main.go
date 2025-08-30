ee09e7a74818fd6231feb38685b4d6f7d24be116
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-30 11:50:49 +0100

    Segmented TUI entry and UFW command testing entry

--- main.go
+++ main.go
@@ -1,36 +1,27 @@
 package main
 
 import (
+	"TUFWGo/system"
 	"TUFWGo/tui"
-	"fmt"
-	"os"
-
-	"github.com/charmbracelet/x/term"
+	"flag"
 )
 
+var skipTermCheck = flag.Bool("skip-term-check", false, "Skip the terminal size check")
+
 func main() {
-	if !termCheck() {
+	//runTUIMode()
+	system.Input()
+}
+
+func runTUIMode() {
+	flag.Parse()
+	if *skipTermCheck {
+		tui.RunTUI()
+		return
+	}
+
+	if !system.TermCheck() {
 		return
 	}
 	tui.RunTUI()
-	//system.Input()
-}
-
-// Enforce terminal check at the very beginning and ensure all users maximise their terminal window before using the TUI
-func termCheck() bool {
-	w, h, err := getTermSize()
-	if err != nil {
-		fmt.Println("Unable to get terminal size:", err)
-		return false
-	}
-	safeW, safeH := 209, 51
-	if w < safeW || h < safeH {
-		fmt.Printf("Your terminal size is too small (%dx%d). Please maximise terminal window!\n", w, h)
-		return false
-	}
-	return true
-}
-
-func getTermSize() (width, height int, err error) {
-	return term.GetSize(uintptr(int(os.Stdout.Fd())))
 }
