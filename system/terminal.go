57f7814f6e57ba72de6334cbcf5101dbcb088aeb
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-08-30 11:51:01 +0100

    Outsourced terminal check logic

---
+++ system/terminal.go
@@ -0,0 +1,26 @@
+package system
+
+import (
+	"fmt"
+	"github.com/charmbracelet/x/term"
+	"os"
+)
+
+// TermCheck Enforce terminal check at the very beginning and ensure all users maximise their terminal window before using the TUI
+func TermCheck() bool {
+	w, h, err := getTermSize()
+	if err != nil {
+		fmt.Println("Unable to get terminal size:", err)
+		return false
+	}
+	safeW, safeH := 209, 51
+	if w < safeW || h < safeH {
+		fmt.Printf("Your terminal size is too small (%dx%d). Please maximise terminal window!\n", w, h)
+		return false
+	}
+	return true
+}
+
+func getTermSize() (width, height int, err error) {
+	return term.GetSize(uintptr(int(os.Stdout.Fd())))
+}
