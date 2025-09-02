f8ad4286decce9517d47d01665576b7c5d883789
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-09-02 15:04:11 +0100

    support for stdin when conversation with terminal output is required

--- system/sys-pipe.go
+++ system/sys-pipe.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func prepareCommand(cmdStr string) (string, error) {
@@ -20,8 +21,34 @@ func prepareCommand(cmdStr string) (string, error) {
 	return string(output), nil
 }
 
+func prepareCommandConversation(cmdStr, input string) (string, error) {
+	cmd := exec.Command("bash", "-c", cmdStr)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if input != "" {
+		cmd.Stdin = strings.NewReader(input)
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New(fmt.Sprint("stderr:", stderr.String()))
+	}
+	return stdout.String(), nil
+}
+
 func RunCommand(command string) (string, error) {
 	out, err := prepareCommand(command)
+	if err != nil {
+		return "", err
+	}
+	return out, nil
+}
+
+func CommandConversation(command, reply string) (string, error) {
+	out, err := prepareCommandConversation(command, reply)
 	if err != nil {
 		return "", err
 	}
