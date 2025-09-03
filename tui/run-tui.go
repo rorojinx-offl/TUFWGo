49c634fcd392244f391a74c4a31161175f4e9a41
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-09-03 10:16:42 +0100

    simple menu content changes with SSH status

--- tui/run-tui.go
+++ tui/run-tui.go
@@ -8,8 +8,15 @@ var SSHActive = false
 
 func RunTUI() {
 	tabs := []string{"General", "IPv6 Mode", "Profile Management", "Settings"}
+	var withSSH []string
+	if SSHActive {
+		withSSH = []string{"List Current Rules", "Add Rule", "Remove Rule", "Test SSH Connection, Fail2Ban Dashboard (Coming Soon!)"}
+	} else {
+		withSSH = []string{"List Current Rules", "Add Rule", "Remove Rule", "Fail2Ban Dashboard (Coming Soon!)"}
+	}
+
 	tabContent := []*Model{
-		{Items: []string{"List Current Rules", "Add Rule", "Remove Rule", "Test SSH Connection"}},
+		{Items: withSSH},
 		{Items: []string{"Adjust your preferences here.", "Change settings as needed.", "Customize your experience.", "Save your changes."}},
 		{Items: []string{"View and edit your profile information.", "Manage your account details.", "Update your password.", "Set your privacy options."}},
 		{Items: []string{"Find answers to common questions.", "Contact support if needed.", "Explore tutorials and guides.", "Get the most out of the app."}},
