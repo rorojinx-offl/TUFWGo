f787d226969ee832fcba65acf34a88ea85ac9f2c
rohitg-city <Rohit.Gurunathan@city.ac.uk>
2025-09-05 17:30:19 +0100

    Concocting the mail format

--- alert/mail-sample.go
+++ alert/mail-sample.go
@@ -9,11 +9,31 @@ import (
 )
 
 func SendSampleMail() {
-	from := mail.NewEmail("Example User", "alerts@em2695.tufwgo.store")
-	subject := "Sending with Twilio SendGrid is Fun"
-	to := mail.NewEmail("Example User", "rohitgurunathan@gmail.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	from := mail.NewEmail("Alert Manager", "alerts@em2695.tufwgo.store")
+	subject := "[TUFWGo] Rule Added - Allow TCP 22 from 192.168.1.1"
+	to := mail.NewEmail("Rohit Gurunathan", "rohitgurunathan@gmil.com")
+	plainTextContent := `Hello,a
+	An action was performed on your firewall via TUFWGo.
+	📌 Action: Rule Added
+	📌 Status: Success
+	📌 Timestamp: 2024-10-05 14:30:00
+	📌 Executed By: root
+	📌 Hostname: raspberrypi
+	📌 Rule Details:
+		- Action: Allow
+		- Direction: Inbound
+		- From: 192.168.1.1
+		- To: Any
+		- Port: 22
+		- Protocol: TCP
+		- App Profile: N/A
+
+	🏷️ Command Executed:
+		ufw allow from 192.168.1.5 to any port 22 proto tcp
+
+	TUFWGo Alert Manager
+	`
+	htmlContent := fmt.Sprintf("<strong>%s</strong>", plainTextContent)
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
