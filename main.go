ec9e94f84339dd17965a882a3a4e71b30d896ad2
Rohit <rohitgurunathan@gmail.com>
2025-09-13 20:10:34 +0100

    new setup check is performed to ensure that all required dirs and helper bins are present in the filesystem, if not create relevant dirs and download relevant bins.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"TUFWGo/tui"
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 var skipTermCheck = flag.Bool("skip-term-check", false, "Skip the terminal size check")
@@ -14,11 +16,12 @@ var sshMode = flag.Bool("ssh", false, "Run in SSH mode")
 
 func main() {
 	local.RequireRoot()
-	runTUIMode()
+	//runTUIMode()
 	//samples.Input()
 	//tui.RunForm()
 	//alert.SendSampleMail()
 	//tui.RunCreateProfile()
+	initSetup()
 }
 
 func runTUIMode() {
@@ -68,4 +71,95 @@ func runTUIMode() {
 		return
 	}
 	tui.RunTUI()
+}
+
+func initSetup() {
+	initDone := false
+	cfgDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println("Failed to get user config dir:", err)
+		return
+	}
+	baseCfgPath := filepath.Join(cfgDir, "tufwgo")
+	authController := filepath.Join(baseCfgPath, "authorised_controllers.json")
+	pdcDir := filepath.Join(baseCfgPath, "pdc")
+	pdcLogs := filepath.Join(pdcDir, "logs")
+	pdcBin := filepath.Join(pdcDir, "tufwgo-deploy")
+	profilesDir := filepath.Join(baseCfgPath, "profiles")
+	authBin := "/usr/bin/tufwgo-auth"
+
+	if _, err = os.Stat(baseCfgPath); err != nil {
+		fmt.Println("TUFWGo config not found, creating config folder...")
+		err = os.MkdirAll(baseCfgPath, 0700)
+		if err != nil {
+			fmt.Println("Failed to create config dir:", err)
+			return
+		}
+		fmt.Println("Config folder created at", baseCfgPath)
+	}
+
+	if _, err = os.Stat(authController); err != nil {
+		fmt.Println("Authorised controllers file not found, creating...")
+		_, err = os.Create(authController)
+		if err != nil {
+			fmt.Println("Failed to create authorised controllers file:", err)
+			return
+		}
+		fmt.Println("Authorised controllers file created at", authController)
+	}
+
+	if _, err = os.Stat(pdcDir); err != nil {
+		fmt.Println("Profile Distribution Center directory not found, creating...")
+		err = os.MkdirAll(pdcDir, 0700)
+		if err != nil {
+			fmt.Println("Failed to create PDC directory:", err)
+			return
+		}
+		fmt.Println("PDC directory created at", pdcDir)
+	}
+
+	if _, err = os.Stat(pdcLogs); err != nil {
+		fmt.Println("PDC logs directory not found, creating...")
+		err = os.MkdirAll(pdcLogs, 0700)
+		if err != nil {
+			fmt.Println("Failed to create PDC logs directory:", err)
+			return
+		}
+		fmt.Println("PDC logs directory created at", pdcLogs)
+	}
+
+	if _, err = os.Stat(pdcBin); err != nil {
+		fmt.Println("PDC binary not found, downloading...")
+		err = local.DownloadFile("https://txrijwxmwfoempqmsuva.supabase.co/storage/v1/object/public/deploy%20binary/tufwgo-deploy", pdcBin, "2cc0d076b24b354fcb451e94d5b6aaf040f267719d6926426a2b7c462d96ea97")
+		if err != nil {
+			fmt.Println("Failed to download PDC binary:", err)
+			return
+		}
+		fmt.Println("PDC binary downloaded at", pdcBin)
+	}
+
+	if _, err = os.Stat(profilesDir); err != nil {
+		fmt.Println("Profiles directory not found, creating...")
+		err = os.MkdirAll(profilesDir, 0700)
+		if err != nil {
+			fmt.Println("Failed to create profiles directory:", err)
+			return
+		}
+		fmt.Println("Profiles directory created at", profilesDir)
+	}
+
+	if _, err = os.Stat(authBin); err != nil {
+		fmt.Println("Auth binary not found at /usr/bin/tufwgo-auth, downloading...")
+		err = local.DownloadFile("https://txrijwxmwfoempqmsuva.supabase.co/storage/v1/object/public/deploy%20binary/tufwgo-auth", authBin, "15361a36a6b533e990cbbb99c631dc2a645bfe553a4e25226a2b8a40c4b5c6b9")
+		if err != nil {
+			fmt.Println("Failed to download auth binary:", err)
+			return
+		}
+		fmt.Println("Auth binary downloaded at", authBin)
+		initDone = true
+	}
+	if initDone {
+		fmt.Println("Initial setup completed.")
+	}
+	runTUIMode()
 }
