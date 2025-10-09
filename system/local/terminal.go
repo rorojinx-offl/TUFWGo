73473082dc5d8c157c6bb677bc85109791275ab4
Rohit Gurunathan <rohitgurunathan@gmail.com>
2025-10-09 11:06:29 +0100

    minor adjustment to terminal size constraints

--- system/local/terminal.go
+++ system/local/terminal.go
@@ -2,8 +2,9 @@ package local
 
 import (
 	"fmt"
-	"github.com/charmbracelet/x/term"
 	"os"
+
+	"github.com/charmbracelet/x/term"
 )
 
 // TermCheck Enforce terminal check at the very beginning and ensure all users maximise their terminal window before using the TUI
@@ -13,7 +14,7 @@ func TermCheck() bool {
 		fmt.Println("Unable to get terminal size:", err)
 		return false
 	}
-	safeW, safeH := 209, 47
+	safeW, safeH := 206, 47
 	if w < safeW || h < safeH {
 		fmt.Printf("Your terminal size is too small (%dx%d). Please maximise terminal window!\n", w, h)
 		return false
