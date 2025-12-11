4c677d8f370d4ac883de2c4561452939a62e8394
Rohit Gurunathan <rohitgurunathan@gmail.com>
2025-12-11 23:19:32 +0000

    resolves package manager on system

--- system/local/pkg.go
+++ system/local/pkg.go
@@ -7,16 +7,14 @@ type PackageManager string
 const (
 	APT     PackageManager = "apt"
 	DNF     PackageManager = "dnf"
-	YUM     PackageManager = "yum"
 	PACMAN  PackageManager = "pacman"
 	ZYPPER  PackageManager = "zypper"
-	NIX     PackageManager = "nix"
 	APK     PackageManager = "apk"
 	UNKNOWN PackageManager = "unknown"
 )
 
 func DetectPkgMgr() PackageManager {
-	list := []PackageManager{APT, DNF, YUM, PACMAN, ZYPPER, NIX, APK}
+	list := []PackageManager{APT, DNF, PACMAN, ZYPPER, APK}
 	for _, m := range list {
 		if _, err := exec.LookPath(string(m)); err != nil {
 			continue
