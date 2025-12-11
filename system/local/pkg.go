4db23f957b2f26a712946e691f27f765717edfaa
Rohit Gurunathan <rohitgurunathan@gmail.com>
2025-12-11 20:07:57 +0000

    package manager resolution for automated dependency installation

---
+++ system/local/pkg.go
@@ -0,0 +1,25 @@
+package local
+
+import "os/exec"
+
+type PackageManager string
+
+const (
+	APT     PackageManager = "apt"
+	DNF     PackageManager = "dnf"
+	YUM     PackageManager = "yum"
+	PACMAN  PackageManager = "pacman"
+	ZYPPER  PackageManager = "zypper"
+	NIX     PackageManager = "nix"
+	UNKNOWN PackageManager = "unknown"
+)
+
+func DetectPkgMgr() PackageManager {
+	list := []PackageManager{APT, DNF, YUM, PACMAN, ZYPPER, NIX}
+	for _, m := range list {
+		if _, err := exec.LookPath(string(m)); err != nil {
+			return m
+		}
+	}
+	return UNKNOWN
+}
